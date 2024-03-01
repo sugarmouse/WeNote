1e892b0a357112f5a58a07034f69f24e1726b89d
sugarmouse <tanghao19950911@gmail.com>
2024-03-01 17:07:59 +0800

    delete useless file and init

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p className="text-red-500 font-bold">notion clone</p>
+      <Button variant="ghost">test button</Button>
+    </div>
+  );
+}
